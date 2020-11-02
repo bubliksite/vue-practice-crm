fc1d32567b5bf10fb2849219c511fd48cbf45c31
bubliksite <serj.solntsev@gmail.com>
2020-11-03 00:42:06 +0300

    Second commit (#12)

--- src/store/category.js
+++ src/store/category.js
@@ -2,6 +2,25 @@ import firebase from "firebase/app";
 
 export default {
   actions: {
+    async fetchCategories({ commit, dispatch }) {
+      try {
+        const uid = await dispatch("getUid");
+        const categories =
+          (
+            await firebase
+              .database()
+              .ref(`/users/${uid}/categories`)
+              .once("value")
+          ).val() || {};
+        return Object.keys(categories).map(key => ({
+          ...categories[key],
+          id: key
+        }));
+      } catch (e) {
+        commit("setError", e);
+        throw e;
+      }
+    },
     async addCategory({ commit, dispatch }, { title, limit }) {
       try {
         const uid = await dispatch("getUid");
@@ -10,6 +29,19 @@ export default {
           .ref(`/users/${uid}/categories`)
           .push({ title, limit });
         return { title, limit, id: category.key };
+      } catch (e) {
+        commit("setError", e);
+        throw e;
+      }
+    },
+    async updateCategory({ commit, dispatch }, { title, limit, id }) {
+      try {
+        const uid = await dispatch("getUid");
+        await firebase
+          .database()
+          .ref(`/users/${uid}/categories`)
+          .child(id)
+          .update({ title, limit });
       } catch (e) {
         commit("setError", e);
         throw e;
